c943a6fdd73d83fd9b82e224dba7cd67f626ebc0
JOSE ANTONIO CRUZ <nickcaos1234585@gmail.com>
2026-03-16 12:01:24 -0400

    Add temperature evaluation script

---
+++ ejercicio6.py
@@ -0,0 +1,9 @@
+# ESTUDIANTE : JOSE ANTONIO CRUZ ALCON PARALELO: 7C MATERIA: EMERGENTES II
+temperatura = 18
+
+if temperatura > 28:
+    print("Hace calor")
+elif temperatura > 20:
+    print("Es un dia agradable")
+else:
+    print("Hace frio")
