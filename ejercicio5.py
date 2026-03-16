2cf2d0d83a1381f0840fd1202f179b41fe399918
JOSE ANTONIO CRUZ <nickcaos1234585@gmail.com>
2026-03-16 12:01:03 -0400

    Add ejercicio5.py for summing two numbers

---
+++ ejercicio5.py
@@ -0,0 +1,8 @@
+# ESTUDIANTE : JOSE ANTONIO CRUZ ALCON PARALELO: 7C MATERIA: EMERGENTES II
+num1 = input("Ingresa el primer número: ")
+num2 = input("Ingresa el segundo número: ")
+
+# Se convierte de string a entero para poder sumar matemáticamente
+suma = int(num1) + int(num2) 
+
+print("La suma es: ", suma)
