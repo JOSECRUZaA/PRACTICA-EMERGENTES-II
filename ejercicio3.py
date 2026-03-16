e4fe0d35c6fd20c43a09d91094d470fbf7bf990e
JOSE ANTONIO CRUZ <nickcaos1234585@gmail.com>
2026-03-16 11:58:46 -0400

    Add ejercicio3.py with string manipulation examples

---
+++ ejercicio3.py
@@ -0,0 +1,8 @@
+# ESTUDIANTE : JOSE ANTONIO CRUZ ALCON PARALELO: 7C MATERIA: EMERGENTES II
+curso = "Python para iniciantes"
+
+print(curso.upper())       # Convierte a mayúsculas
+print(curso.lower())       # Convierte a minúsculas
+print(curso.capitalize())  # Pone la primera letra en mayúscula
+print(curso.find("h"))     # Encuentra la posición de la letra "h"
+print(curso.replace("para", "FOR")) # Reemplaza una palabra por otra
