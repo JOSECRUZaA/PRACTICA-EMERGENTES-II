aca3a97550dd1cf23c5b56e421b156f57c0eeec3
JOSE ANTONIO CRUZ <nickcaos1234585@gmail.com>
2026-03-16 12:08:12 -0400

    Add list operations in ejercicio8.py
    Initial implementation of list operations in ejercicio8.py.

---
+++ ejercicio8.py
@@ -0,0 +1,9 @@
+# ESTUDIANTE : JOSE ANTONIO CRUZ ALCON PARALELO: 7C MATERIA: EMERGENTES II
+numeros = [1, 2, 3, 4, 5]
+
+numeros.append(6)       # Agrega un elemento al final
+numeros.insert(0, -1)   # Inserta el -1 en la posición 0
+numeros.remove(1)       # Elimina el elemento con valor 1
+# numeros.clear()       # Elimina todo el contenido de la lista
+
+print(numeros)
